e323dbab74fe29e7db173108592a104140867a1a
Agustt123 <agustintracheskyoficial@gmail.com>
2026-03-04 14:36:48 -0300

    dasdsa

--- controller/auth/login_empresa.js
+++ controller/auth/login_empresa.js
@@ -1,6 +1,7 @@
 import crypto from "crypto";
 import { CustomException, generateToken, LightdataORM, Status } from "lightdata-tools";
 import { companiesService, jwtAudience, jwtIssuer, jwtSecret } from "../../db.js";
+import { LightdataORMFix } from "../../src/ormFix.js";
 
 export async function loginEmpresa({ db, req }) {
     const { companyCode } = req.body;
@@ -10,13 +11,25 @@ export async function loginEmpresa({ db, req }) {
     const company = await companiesService.getByCode(companyCode);
 
 
-    const [sistemaData] = await LightdataORM.select({
+    const [sistemaData] = await LightdataORMFix.select({
         db,
         table: "sistema_empresa",
         where: {
             did: company.did,
         }
+
+
     });
+
+    if (
+        !sistemaData
+    ) {
+        throw new CustomException({
+            title: "Empresa no encontrada",
+            message: "La empresa no se encuentra registrada en el sistema",
+            status: Status.notFound,
+        });
+    }
 
     return {
         success: true,
